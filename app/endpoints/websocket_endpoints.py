303c313349c801e87257e51402e8b3ddaa9b72ac
juanpluduena <juan.luduena.161@mi.unc.edu.ar>
2024-09-30 01:13:58 -0300

    CTRLZ-37 added authorization to all game endpoints

--- app/endpoints/websocket_endpoints.py
+++ app/endpoints/websocket_endpoints.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, HTTPException, Depends, WebSocket, WebSocketDisconnect
-from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends, WebSocket, WebSocketDisconnect, status
 from sqlalchemy import event
 from sqlalchemy.orm import Session
 from app.db.db import get_db
 from app.models.game_models import Game
 from app.services.websocket_services import GameListManager, GameConnectionsManager
+from app.services.auth_services import verify_token_in_db
 import asyncio
 import logging
 import threading
@@ -13,55 +13,96 @@ router = APIRouter()
 game_list_manager = GameListManager()
 game_connection_manager = GameConnectionsManager()
 
+
 def start_event_loop(loop):
     asyncio.set_event_loop(loop)
     loop.run_forever()
 
+
 def get_or_create_event_loop():
     try:
         loop = asyncio.get_running_loop()
-        #logging.debug(f"Is the event loop that I got running? {loop.is_running()}")
+        # logging.debug(f"Is the event loop that I got running? {loop.is_running()}")
         return loop
     except RuntimeError:
         loop = asyncio.new_event_loop()
-        threading.Thread(target=start_event_loop, args=(loop,), daemon=True).start()
-        #logging.debug(f"Is the event loop that I created running? {loop.is_running()}")
+        threading.Thread(target=start_event_loop,
+                         args=(loop,), daemon=True).start()
+        # logging.debug(f"Is the event loop that I created running? {loop.is_running()}")
         return loop
+
 
 @event.listens_for(Game, 'after_insert')
 def handle_creation(mapper, connection, target: Game):
     loop = get_or_create_event_loop()
     logging.debug(f"About to broadcast game added: {target}")
-    asyncio.run_coroutine_threadsafe(game_list_manager.broadcast_game("game added", target), loop)
+    asyncio.run_coroutine_threadsafe(
+        game_list_manager.broadcast_game("game added", target), loop)
+
 
 @event.listens_for(Game, 'after_delete')
 def handle_deletion(mapper, connection, target: Game):
     loop = get_or_create_event_loop()
     logging.debug(f"About to broadcast game deleted: {target}")
-    asyncio.run_coroutine_threadsafe(game_list_manager.broadcast_game("game deleted", target), loop)
+    asyncio.run_coroutine_threadsafe(
+        game_list_manager.broadcast_game("game deleted", target), loop)
+
 
 @event.listens_for(Game, 'after_update')
 def handle_change(mapper, connection, target: Game):
     loop = get_or_create_event_loop()
     logging.debug(f"About to broadcast game updated: {target}")
-    asyncio.run_coroutine_threadsafe(game_list_manager.broadcast_game("game updated", target), loop)
+    asyncio.run_coroutine_threadsafe(
+        game_list_manager.broadcast_game("game updated", target), loop)
 
 
 @router.websocket("/ws/list")
 async def list(websocket: WebSocket, db: Session = Depends(get_db)):
-    await game_list_manager.connect(websocket)
+    db: Session = next(get_db())
+
+    # extract token from header `Authorization`
+    token = websocket.headers.get("Authorization")
+
+    if token is None or not token.startswith("Bearer "):
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
+
+    token = token.split(" ")[1]  # delete prefix "Bearer"
+
     try:
-        while True:
-            data = await websocket.receive_text() 
-    except WebSocketDisconnect:
-        await game_list_manager.disconnect(websocket)
+        await verify_token_in_db(token, db)
+        await game_list_manager.connect(websocket)
+        try:
+            while True:
+                data = await websocket.receive_text()
+        except WebSocketDisconnect:
+            await game_list_manager.disconnect(websocket)
+    except HTTPException as e:
+        # close connection if token is invalid
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
 
 
 @router.websocket("/ws/game/{game_id}")
 async def game(websocket: WebSocket, game_id: int, db: Session = Depends(get_db)):
-    await game_connection_manager.connect(websocket, game_id)
+    db: Session = next(get_db())
+
+    # extract token from header `Authorization`
+    token = websocket.headers.get("Authorization")
+
+    if token is None or not token.startswith("Bearer "):
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
+
+    token = token.split(" ")[1]  # delete prefix "Bearer"
+
     try:
-        while True:
-            data = await websocket.receive_text()
-    except WebSocketDisconnect:
-        await game_list_manager.disconnect(websocket)
+        await verify_token_in_db(token, db)
+        await game_connection_manager.connect(websocket, game_id)
+        try:
+            while True:
+                data = await websocket.receive_text()
+        except WebSocketDisconnect:
+            await game_list_manager.disconnect(websocket)
+    except HTTPException as e:
+        # close connection if token is invalid
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
--- app/services/game_services.py
+++ app/services/game_services.py
@@ -7,7 +7,6 @@ from app.schemas.player_schemas import PlayerGameSchemaOut
 import random
 
 
-
 def validate_game_capacity(game: Game):
     """validates if the player can join the game based on the capacity set by the host"""
     if len(game.players) >= game.player_amount:
@@ -28,27 +27,25 @@ def convert_game_to_schema(game: Game) -> GameSchemaOut:
                              host_id=game.host_id, player_turn=game.player_turn, status=game.status)
 
 
-def search_player_in_game(id_player: int, game: Game) -> Player:
+def search_player_in_game(player: Player, game: Game) -> Player:
     """
     Searchs for a player inside the game.
     Handle exception if the player is not inside the game.
     """
-    player = next(
-        (item for item in game.players if item.id == id_player), None)
-
-    if not player:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="El jugador no esta en la partida")
-
-    return player
+    for pl in game.players:
+        if pl == player:
+            break
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="El jugador no esta en la partida")
 
 
-def is_player_host(id_player: int, game: Game) -> bool:
+def is_player_host(player: Player, game: Game) -> bool:
     """
     Checks if the player is the game host.
     Returns true if the player is the game host, if not returns false.
     """
-    return id_player == game.host_id
+    return player.id == game.host_id
 
 
 def update_game_in_db(db: Session, game: Game):
@@ -81,14 +78,14 @@ def convert_game_to_schema(game: Game) -> GameSchemaOut:
         id=pl.id, name=pl.name) for pl in game.players]
     return game_out
 
-def validate_players_amount(game:Game):
+
+def validate_players_amount(game: Game):
     if len(game.players) != game.player_amount:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail="La partida requiere la cantidad de jugadores especificada para ser iniciada")
-    
-def shuffle_players (game:Game):
 
-    random.shuffle (game.players)
-    game.player_turn=0
-    
-    
+
+def shuffle_players(game: Game):
+
+    random.shuffle(game.players)
+    game.player_turn = 0

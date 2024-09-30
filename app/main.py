c01de0a8535bcccbe6ffdc8448057f922958ad43
Dianefz <dianela.fernandez@mi.unc.edu.ar>
2024-09-30 13:52:05 -0300

    CTRLZ-46 OPTIONS problem solved

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.endpoints import game_endpoints, player_endpoints, websocket_endpoints
 from app.db.db import Base, engine
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,7 +11,13 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="El Switcher API documentation",
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""], 
+    allow_credentials=True,
+    allow_methods=[""],  
+    allow_headers=[""],  
+)
 app.include_router(router=game_endpoints.router)
 app.include_router(router=player_endpoints.router)
 app.include_router(router=websocket_endpoints.router)
